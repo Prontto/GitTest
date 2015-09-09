e2304fb9c60044383f78df4321c39e7b13fe3c2e
Prontto <tuominen.samu@gmail.com>
2015-09-09 17:06:42 +0300

    Uusi projekti lisätty.

---
+++ Testing/Testing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Samu Tuominen on 9.9.2015.
+//  Copyright © 2015 Samu Tuominen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
